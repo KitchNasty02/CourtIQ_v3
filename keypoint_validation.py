ad04cba461d69880fd2d39536e38edaad26479ac
KitchNasty02 <connorkitch10@gmail.com>
2026-02-15 22:53:31 -0600

    added keypoint validation

---
+++ keypoint_validation.py
@@ -0,0 +1,108 @@
+from models.keypoint_head import CourtKeypointHead
+from data.keypoint_dataset import CourtKeypointDataset
+from utils.generate_heatmaps import generate_heatmaps_batch
+from utils.heatmap_to_keypoints import heatmaps_to_keypoints
+
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch
+import timm
+
+
+def validate():
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Validating on {device}")
+
+    # backbone
+    backbone = timm.create_model(
+        'mobilevit_s',
+        pretrained=False,
+        features_only=True
+    )
+    backbone.to(device)
+
+    # compute stride (should be 32)
+    with torch.no_grad():
+        x = torch.zeros(1, 3, 720, 1280).to(device)
+        f = backbone(x)[-1]
+        stride = x.shape[-1] // f.shape[-1]
+
+    # keypoint head
+    head = CourtKeypointHead(
+        in_channels=f.shape[1],
+        num_keypoints=14
+    )
+    head.to(device)
+
+    # load weights
+    checkpoint = torch.load(
+        "weights/finetune_epoch_20.pth",
+        map_location=device
+    )
+
+    backbone.load_state_dict(checkpoint["backbone"])
+    head.load_state_dict(checkpoint["head"])
+
+    # set to evaluation mode
+    backbone.eval()
+    head.eval()
+
+
+    # validation dataset
+    val_dataset = CourtKeypointDataset(
+        img_dir="keypoint_data/images",
+        label_file="keypoint_data/data_val.json"
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=8,   # could try different batch size
+        shuffle=False,
+        num_workers=2,
+        pin_memory=True
+    )
+
+    with torch.no_grad():
+
+        for i, (images, keypoints) in enumerate(val_loader):
+
+            images = images.to(device)
+
+            features = backbone(images)
+            last_feat = features[-1]
+            pred_heatmaps = head(last_feat)
+
+            img_size = images[0].shape[1:]
+            pred_keypoints = heatmaps_to_keypoints(pred_heatmaps, img_size, stride)
+
+            gt_keypoints = keypoints[0].cpu().numpy()
+
+            # move image to cpu for plotting
+            img = images[0].cpu().permute(1, 2, 0).numpy()
+
+            plt.figure(figsize=(10, 6))
+            plt.imshow(img)
+            # plt.imshow(pred_heatmaps[0,0].cpu(), alpha=0.5) # shows heatmap at the first keypoint
+
+            plt.title("Predicted Keypoints")
+
+            # red predicted, green truth
+            for (x, y) in pred_keypoints[0]:
+                plt.scatter(x, y, c='red', s=40)
+
+            for (x, y) in gt_keypoints:
+                plt.scatter(x, y, c='green', s=40)
+
+            plt.show()
+
+
+
+
+
+
+if __name__ == "__main__":
+    validate()
+
+
---
+++ utils/heatmap_to_keypoints.py
@@ -0,0 +1,68 @@
+import torch.nn.functional as F
+import torch
+
+def heatmaps_to_keypoints(heatmaps, img_size, stride):
+    B, K, H_hm, W_hm = heatmaps.shape
+    H_img, W_img = img_size
+    keypoints = []
+
+    for b in range(B):
+        kp_list = []
+        for k in range(K):
+            heatmap = heatmaps[b, k]
+            
+            # get index of max activation
+            flat_idx = heatmap.argmax()
+            y = flat_idx // W_hm
+            x = flat_idx % W_hm
+            
+            x = x.item()
+            y = y.item()
+            
+            
+            """
+            Subpixel refinement using Taylor expansion
+
+                Imagine you have a heatmap peak at position `x=12`. The values around it are:
+                    
+                    heatmap[y, 11] = 0.3
+                    heatmap[y, 12] = 0.9  <-- peak
+                    heatmap[y, 13] = 0.5
+
+                The true peak is probably not exactly at x=12, but somewhere between 11 and 13.
+
+                Model the heatmap locally as a quadratic function
+                
+                    h(x) ≈ h(x₀) + h'(x₀)·Δx + ½h''(x₀)·Δx²
+            """
+            if 1 <= x < W_hm-1 and 1 <= y < H_hm-1:
+                # get neighboring values
+                # curvature (second derivative)
+                dxx = (heatmap[y, x+1] - 2*heatmap[y, x] + heatmap[y, x-1]).item()
+                dyy = (heatmap[y+1, x] - 2*heatmap[y, x] + heatmap[y-1, x]).item()
+
+                # gradient (first derivative)
+                dx = (heatmap[y, x+1] - heatmap[y, x-1]).item() * 0.5
+                dy = (heatmap[y+1, x] - heatmap[y-1, x]).item() * 0.5
+                
+                # quadratic peak estimation
+                if dxx < 0:  # ensure it's a peak (concave down), not valley
+                    x = x - dx / dxx    # taylor expansion formula
+                if dyy < 0:
+                    y = y - dy / dyy
+            
+            # convert to image coordinates
+            x = x * stride
+            y = y * stride
+
+            # clamp to image bounds
+            x = min(max(x, 0), W_img - 1)
+            y = min(max(y, 0), H_img - 1)
+
+            kp_list.append([x, y])
+
+        keypoints.append(kp_list)
+
+    return keypoints
+
+
