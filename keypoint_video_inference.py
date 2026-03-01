80bd708875be67b4cf12c9061da92f2c010f41ae
KitchNasty02 <connorkitch10@gmail.com>
2026-02-28 23:26:00 -0600

    added keypoint prediction averaging

--- keypoint_video_inference.py
+++ keypoint_video_inference.py
@@ -3,9 +3,12 @@ import torch
 import yt_dlp
 import timm
 import os
+import numpy as np
 
 from models.keypoint_head import CourtKeypointHead
 from utils.heatmap_to_keypoints import heatmaps_to_keypoints
+
+MOVING_AVERAGE_NUM = 5  # 5 point moving average
 
 
 def download_youtube_video(url, output_path="youtube_video.mp4"):
@@ -20,6 +23,21 @@ def download_youtube_video(url, output_path="youtube_video.mp4"):
         ydl.download([url])
     
     return output_path
+
+
+def calculate_keypoint_moving_average(pred_history, new_pred):
+
+    # skip if not enough pred yet
+    if len(pred_history) < MOVING_AVERAGE_NUM:
+        pred_history.append(new_pred)
+        return new_pred, pred_history
+    
+    pred_history.pop(0)     # remove oldest prediction
+    pred_history.append(new_pred)
+
+    avg_pred = np.mean(pred_history, axis=0)
+    
+    return avg_pred, pred_history
 
 
 def predict_video(video_source=0, output_path=None):
@@ -80,6 +98,10 @@ def predict_video(video_source=0, output_path=None):
     print(f"Processing video: {fps} FPS, {width}x{height}")
     
     frame_count = 0
+
+    inference_history = []
+    last_pred = 0
+    inference_frame_skip = 5
     
     with torch.no_grad():
         while True:
@@ -88,6 +110,23 @@ def predict_video(video_source=0, output_path=None):
                 break
             
             frame_count += 1
+
+            if frame_count % inference_frame_skip != 0 and frame_count > 1:
+                for (x, y) in last_pred:
+                    x_scaled = int(x * scale_x)
+                    y_scaled = int(y * scale_y)
+                    cv2.circle(frame, (x_scaled, y_scaled), 5, (0, 0, 255), -1)
+                
+                # Draw court lines (optional - connect keypoints)
+                draw_court_lines(frame, last_pred, scale_x, scale_y)
+                
+                # Display FPS
+                cv2.putText(frame, f"Frame: {frame_count}", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                
+                if writer:
+                    writer.write(frame)
+                continue
             
             # preprocces image the same as the CourtKeypointDataset
             img_resized = cv2.resize(frame, (1280, 720))  # width, height
@@ -107,18 +146,23 @@ def predict_video(video_source=0, output_path=None):
             img_size = (720, 1280)  # Your training size
             pred_keypoints = heatmaps_to_keypoints(pred_heatmaps, img_size, stride)
             
+
+            # find 5 point moving average
+            avg_pred, inference_history = calculate_keypoint_moving_average(inference_history, pred_keypoints[0])
+            last_pred = inference_history[-1]
+            
             # Scale keypoints back to original frame size
             scale_x = width / 1280
             scale_y = height / 720
             
             # Draw keypoints on frame
-            for (x, y) in pred_keypoints[0]:
+            for (x, y) in avg_pred:
                 x_scaled = int(x * scale_x)
                 y_scaled = int(y * scale_y)
                 cv2.circle(frame, (x_scaled, y_scaled), 5, (0, 0, 255), -1)
             
             # Draw court lines (optional - connect keypoints)
-            draw_court_lines(frame, pred_keypoints[0], scale_x, scale_y)
+            draw_court_lines(frame, avg_pred, scale_x, scale_y)
             
             # Display FPS
             cv2.putText(frame, f"Frame: {frame_count}", (10, 30),
@@ -185,8 +229,8 @@ def predict_youtube_video(youtube_url, output_path="output.mp4"):
 if __name__ == "__main__":
     # predict_video(video_source=0)
     
-    youtube_url = "https://www.youtube.com/watch?v=C4Gl-T2dtss"
-    predict_youtube_video(youtube_url, output_path="output/court_detection_Alcaraz_Novak.mp4")
+    # youtube_url = "https://www.youtube.com/watch?v=C4Gl-T2dtss"
+    # predict_youtube_video(youtube_url, output_path="output/court_detection_Alcaraz_Novak.mp4")
 
     # youtube_url = "https://www.youtube.com/watch?v=I6b69yvtufI"
     # predict_youtube_video(youtube_url, output_path="output/court_detection_Alcaraz_Paul.mp4")
@@ -194,6 +238,11 @@ if __name__ == "__main__":
     # youtube_url = "https://www.youtube.com/watch?v=XOR1EuU-08A"
     # predict_youtube_video(youtube_url, output_path="output/court_detection_Federer_Ivashka.mp4")
 
+    # youtube_url = "https://www.youtube.com/watch?v=OneBfXLVV60"
+    # predict_youtube_video(youtube_url, output_path="output/court_detection_hit_with_John.mp4")
+
+    youtube_url = "https://www.youtube.com/watch?v=0KlllgCrvWo"
+    predict_youtube_video(youtube_url, output_path="output/court_detection_Novak_Zverev.mp4")
 
 
 
